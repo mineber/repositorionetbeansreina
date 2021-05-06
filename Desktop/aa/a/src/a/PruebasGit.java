6688e77bec31fdc34dcdea06ae15eca693e585d8
Mineber <mineber1996@gmail.com>
2021-05-06 12:50:25 +0200

    Calculadora suma y resta

---
+++ Desktop/aa/a/src/a/PruebasGit.java
@@ -0,0 +1,57 @@
+package a;
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Mineber
+ */
+public class PruebasGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       //objeto clase scanner
+        Scanner sc= new Scanner(System.in);
+        System.out.println(" Introduce primner numero de la calculadora: ");
+        // metodo para leer un double por teclado
+        double n1 = sc.nextDouble();
+        System.out.println("Introduce el segundo numero de la calcualdora: ");
+        double n2 = sc.nextDouble();
+    
+        // instancio un objeto de clase calculadora que he creado en archivo aparte con los dos numeros pasados
+        calculadora casio= new calculadora(n1, n2 );
+        
+        int operacion =0;
+        do {
+            System.out.println(" Introduce Operacion a realizar");
+            System.out.println(" 1. suma \n 2. resta \n 3. multiplicacion \n 4. division \n 5. salir");
+            // leo por teclado un entero como opción del menú
+            operacion =sc.nextInt();
+            // segun valor elegido ejecuto método del objeto claculadora
+            switch ( operacion ){
+            case 1: System.out.println( " la suma es "+ casio.suma() ); break;
+            case 2: System.out.println( " la resta es "+ casio.resta() ); break;
+            /*case 3: System.out.println(" la multipilcacion es "+ casio.multiplicacion()); break;
+            case 4: System.out.println(" La division es " + casio.division()); break;*/
+            default: System.out.println(" Debes elegir una opción de 1 a 5");
+            }
+    
+        } while ( operacion != 5 );
+        
+        System.out.println(" Gracias por usar la calculadora...hasta luegooooorrr");
+        
+        sc.close();
+    }
+
+}
+    
+    
+
